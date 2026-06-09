f1de956568a91af7b41e85f074abaf5c99a9edcb
al-aminpatowary1038 <ariyanalamin1038@gmail.com>
2026-06-09 14:06:56 +0600

    Add scripts to calculate the area of a circle and a triangle

---
+++ AreaOfCircle.py
@@ -0,0 +1,4 @@
+pie = 3.1416
+r = float(input("Enter The Circle radius: "))
+result = pie * r*r
+print ("Area of Circle = ",result)
---
+++ AreaOfTriangle.py
@@ -0,0 +1,4 @@
+base = float (input("Enter Triangle Base: "))
+height = float (input("Enter Triangle Height: "))
+result = 0.5 * base * height
+print("Area Of Triangle is ",result)
