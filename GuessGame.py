140ee06cfb7301d5bd8c393d1e19f7e8fe7f0cda
al-aminpatowary1038 <ariyanalamin1038@gmail.com>
2026-06-16 17:59:41 +0600

    feat: add GuessGame.py for a number guessing game

---
+++ GuessGame.py
@@ -0,0 +1,16 @@
+from random import randint
+f=5
+count=0
+for i in range(f) :
+    n= int(input("Enter any number between 1 to 100 :"))
+    randomNum = randint(1,3)
+    if n == randomNum:
+        print("Congratulation you guess is correct")
+        print("\n")
+        count = count+1
+    else:
+        print("You have lost")
+        print(f"Random Number is {randomNum}")
+        print("\n")
+print("Your Limit is over")
+print(f"You Succfully Guess {count} times")
